399ffa8137c27a3e85a18550fda47e8fe4fd5c81
Guillaume GENCE <guillaume.gence@gmail.com>
2017-02-09 16:54:05 +0100

    ...

--- modules/parse.js
+++ modules/parse.js
@@ -2,7 +2,7 @@ var exports = module.exports = {};
 exports.parse = parse;
 
 var xml_digester = require("xml-digester");
-var d3hierarchy = require('d3-hierarchy');
+
 var handler = new xml_digester.OrderedElementsHandler("eventType");
 var options = {
   "handler": [{
@@ -32,77 +32,3 @@ function parse(xmlstr,callback) {
     callback(err,recTree);
   });
 }
-
-
-
-function createNewSubTreeWithChild(nodeName,nodeEvent,childName,childEvent) {
-  return {
-    subName : nodeName,
-    event : nodeEvent,
-    clade : [
-      {
-        subName: childName,
-        event : {eventType: 'leaf'},
-        loss : true
-      }
-    ]
-  }
-}
-
-function createNewSubTree(nodeName,nodeEvent) {
-  return {
-    subName : nodeName,
-    event : nodeEvent,
-    clade : []
-  }
-}
-
-// function traverseTree(treeRoot) {
-//   cladeUtil.preOrderTraversal(treeRoot,null,null,function (node,parent,posChild,callback) {
-//
-//     var listEvents = node.eventsRec;
-//     var newEvent = null;
-//
-//
-//     while (listEvents && (newEvent = listEvents.shift()))
-//     {
-//       switch (newEvent.eventType) {
-//         case "speciationLoss":
-//
-//           var nodeName = node.name+"_SpL";
-//           var childName = node.name+"_Loss";
-//           var newParent = createNewSubTreeWithChild(nodeName,newEvent,childName);
-//           cladeUtil.insertSubTree(parent,posChild,newParent);
-//           parent = newParent;
-//           posChild = 1;
-//           break;
-//         case "speciationOutLoss":
-//
-//           var nodeName = node.name+"_SpOL";
-//           var childName = node.name+"_Loss";
-//           var newParent = createNewSubTreeWithChild(nodeName,newEvent,childName);
-//           cladeUtil.insertSubTree(parent,posChild,newParent);
-//           parent = newParent;
-//           posChild = 1;
-//           break;
-//         case "transferBack":
-//
-//           var nodeName = node.name+"_TrB";
-//           var childName = node.name+"_LossD";
-//           var newParent = createNewSubTree(nodeName,newEvent);
-//           cladeUtil.insertSubTree(parent,posChild,newParent);
-//           parent = newParent;
-//           posChild = 1;
-//           break;
-//
-//         default:
-//           node.event = newEvent;
-//
-//       }
-//       delete node.eventsRec;
-//
-//     }
-//
-//   });
-//   return treeRoot;
-// }
