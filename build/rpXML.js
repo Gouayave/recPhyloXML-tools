e3cc50fbf51b8b65027a111e42528911b156d1c7
Guillaume GENCE <guillaume.gence@gmail.com>
2017-02-13 18:12:12 +0100

    change build

--- build/rpXML.js
+++ build/rpXML.js
@@ -19684,7 +19684,7 @@ var defaultConfig = {
 }
 
 function flatTree(treeRoot,config = {}) {
-  
+
   var virtualRoot = {
     name : "Out",
     eventsRec : treeRoot.eventsRec,
@@ -19703,11 +19703,11 @@ function flatTree(treeRoot,config = {}) {
 
         var listEvents = child.data.eventsRec;
         var newEvent = null;
+        var startNode = undefined;
+        var currentNode = undefined;
 
 
-        while (listEvents && (newEvent = listEvents.shift()))
-        {
-
+        while (listEvents && (newEvent = listEvents.shift())){
           switch (newEvent.eventType) {
             case "speciationLoss":
               var newChildName = child.data.name+"_SpL";
@@ -19720,8 +19720,16 @@ function flatTree(treeRoot,config = {}) {
                 var newChild = createNewSubTreeWithChild(newChildName,newEvent,lossChildName,"undefined");
               }
 
-              node.data.clade[posChild] = newChild;
-              newChild.clade.push(child.data);
+              if(!startNode && !currentNode)
+              {
+                startNode = newChild;
+                currentNode = newChild;
+              }else {
+                currentNode.clade.push(newChild);
+                currentNode = newChild;
+              }
+
+
               break;
 
 
@@ -19735,8 +19743,16 @@ function flatTree(treeRoot,config = {}) {
                 else {
                   var newChild = createNewSubTreeWithChild(newChildName,newEvent,lossChildName,newEvent.speciesLocation);
                 }
-                node.data.clade[posChild] = newChild;
-                newChild.clade.push(child.data);
+
+                if(!startNode && !currentNode)
+                {
+                  startNode = newChild;
+                  currentNode = newChild;
+                }else {
+                  currentNode.clade.push(newChild);
+                  currentNode = newChild;
+                }
+
               break;
 
             case "transferBack":
@@ -19749,15 +19765,28 @@ function flatTree(treeRoot,config = {}) {
                 else {
                   var newChild = createNewSubTreeWithChild(newChildName,newEvent,lossChildName,"out");
                 }
-                node.data.clade[posChild] = newChild;
-                newChild.clade.push(child.data);
+
+                if(!startNode && !currentNode)
+                {
+                  startNode = newChild;
+                  currentNode = newChild;
+                }else {
+                  currentNode.clade.push(newChild);
+                  currentNode = newChild;
+                }
+
               break;
 
             default:
               child.data.eventsRec = [newEvent];
-
           }
         }
+        if(startNode && currentNode)
+        {
+          node.data.clade[posChild] = startNode;
+          currentNode.clade.push(child.data);
+        }
+
       });
     }
   });
